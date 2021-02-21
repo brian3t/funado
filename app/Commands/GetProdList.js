edd174ced31ebc8406d096c811ac6cfcb2d4e609
brian3t <ngxtri@gmail.com>
2021-02-20 17:31:12 -0800

    Get prod list done.

--- app/Commands/GetProdList.js
+++ app/Commands/GetProdList.js
@@ -55,22 +55,39 @@ class GetProdList extends Command {
           /** @type Product **/
           let prod_model = await Product.findOrCreate(prod_payload, prod_payload)
           if (! (prod_model instanceof Product)) return 'failed updating Product lucid'
+          prod_model.spfid = product.id
           prod_model.title = product.title
-          const prod_save_res = await prod_model.save()
-          if (! prod_save_res) return 'failed updating product lucid'
+          prod_model.body_html = product.body_html
+          if (!product.variants || !_.isArray(product.variants)) return 'err: missing variants'
+          let prod_var = product.variants[0]
+          prod_model.price = parseInt(prod_var.price)
+          prod_model.sku = prod_var.sku
+          prod_model.fulfillment_service = prod_var.fulfillment_service
+          prod_model.weight = prod_var.weight
+          prod_model.weight_unit = prod_var.weight_unit
+          prod_model.inventory_item_id = prod_var.inventory_item_id
+          prod_model.inventory_quantity = prod_var.inventory_quantity
+          prod_model.requires_shipping = prod_var.requires_shipping
+          prod_model.variantid = prod_var.id
+          if (!product.image || !_.isObject(product.image)) return 'err: missing image'
+          let prod_image = product.image
+          prod_model.img = prod_image.src
+
+          const prod_save_ret = await prod_model.save()
+          if (! prod_save_ret) return 'failed updating product lucid'
         }
         let a = 1
 
       }
       console.log(`After 5 secs we close db, so that process exit; no matter what is running. Time's up!`)
       setTimeout(() => {
-        Database.close()
+        DB.close()
       }, 8000)
     } catch (e) {
       console.error(`error 61: `, e)
     }
     setTimeout(() => {
-      Database.close()
+      DB.close()
     }, 8000)
   }
 }
